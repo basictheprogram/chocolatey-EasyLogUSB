3d2f1f157efdb48ef34c7daad9393ed7ca7e576f
Bob Tanner <tanner@real-time.com>
2019-05-30 11:30:21 -0500

    Minor clean up on camel case variables

--- tools/chocolateyinstall.ps1
+++ tools/chocolateyinstall.ps1
@@ -4,33 +4,33 @@ $packageName = 'EasyLogUSB'
 $toolsDir    = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 $url         = 'https://s3.amazonaws.com/lascar_downloads/EasyLogUSB+Installer.exe' 
 $checkSum    = '134e5dbc3a7489e8b0851c454f9e3330e888752362808cf4fdb5c48d09cf15d0'
-$WorkSpace   = Join-Path $env:TEMP "$packageName.$env:chocolateyPackageVersion"
+$workSpace   = Join-Path $env:TEMP "$packageName.$env:chocolateyPackageVersion"
 
-$WebFileArgs = @{
-  packageName  = $packageName
-  FileFullPath = Join-Path $WorkSpace "$packageName+Installer.exe"
-  Url          = $url
-  Checksum     = $checkSum
-  ChecksumType = 'sha256'
-  GetOriginalFileName = $true
+$webFileArgs = @{
+  packageName         = $packageName
+  FileFullPath        = Join-Path $WorkSpace "$packageName+Installer.exe"
+  url                 = $url
+  checksum            = $checkSum
+  checksumType        = 'sha256'
+  getOriginalFileName = $true
 }
 
-$PackedInstaller = Get-ChocolateyWebFile @WebFileArgs
+$packedInstaller = Get-ChocolateyWebFile @webFileArgs
 
 # Extract the .msi EasyLogUSB+Installer.exe /s /x /b"." /v"/qn"
 #
-$statement = '/s /x /b"' + $WorkSpace + '" /v"/qn"'
-Start-ChocolateyProcessAsAdmin -Statements "$statement" -ExeToRun "$PackedInstaller" -ValidExitCodes @(0,1605)
+$statement = '/s /x /b"' + $workSpace + '" /v"/qn"'
+Start-ChocolateyProcessAsAdmin -Statements "$statement" -ExeToRun "$packedInstaller" -ValidExitCodes @(0,1605)
 
-$InstallArgs = @{
-  PackageName    = $packageName
-  File           = Join-Path $WorkSpace "EasyLog USB.msi"
+$installArgs = @{
+  packageName    = $packageName
+  file           = Join-Path $WorkSpace "EasyLog USB.msi"
   fileType       = 'MSI'
   silentArgs     = "/qb /norestart /l*v `"$($env:TEMP)\$($packageName).$($env:chocolateyPackageVersion).MsiInstall.log`""
   validExitCodes = @(0, 3010, 1641)
 }
 
-Install-ChocolateyInstallPackage @InstallArgs
+Install-ChocolateyInstallPackage @installArgs
 
 # silent install of EasyLog USB Device Driver requires AutoIT
 #
